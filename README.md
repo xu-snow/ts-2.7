17eb50fb042a5961400806e83221a757ea54a319
zhengxu <332171564@qq.com>
2018-05-09 01:47:15 +0800

    update

--- README.md
+++ README.md
@@ -1,7 +1,11 @@
 # TypeScript 2.7版本特性练习
+
 > 针对ts 2.7版本的特性作专门的实例，希望能加深理解。[官方日志文档](http://www.typescriptlang.org/docs/handbook/release-notes/typescript-2-7.html)
 
-### Usage
+## 使用
+
 ``` bash
-npx tsc
+npx tsc --pretty -w
+# 或
+`npm bin`/tsc --pretty -w
 ```
--- src/constant-name-properties/example1.ts
+++ src/constant-name-properties/example1.ts
@@ -6,11 +6,22 @@ import { SERIALIZE, Serializable } from "./lib";
 
 export class JSONSerializableItem implements Serializable {
     //  error TS2420, 只能用引入的SERIALIZE来作属性的名称
-    // ["serialize-method-key"](obj: {}) { //
+    // ["serialize-method-key"](obj: {}) {
     //     return JSON.stringify(obj);
     // }
-    
+
     [SERIALIZE](obj: {}) { //
         return JSON.stringify(obj);
     }
 }
+const Foo = "Foo";
+const Bar = "Bar";
+
+let x = { // 2.7版本之前，x的类型为{[x:string]:string|number},现在为 {[Foo]: number;[Bar]: string;}}
+    [Foo]: 100,
+    [Bar]: "hello",
+};
+
+let a = x[Foo]; // has type 'number'
+let b = x[Bar]; // has type 'string'
+
